7ffbd5d35a9a9a494eeb1e07734f0d8fe9d71f97
Nikolai Krasa <greyrat1@yandex.ru>
2023-09-10 18:39:12 +0300

    поиск решения с токеном 4

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -10,7 +10,7 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 const { PORT = 3000 } = process.env;
